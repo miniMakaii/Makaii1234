4eb67b35d0d1ace726212508fa18b57f7a79f3b1
miniMakaii <stu.makai.wise@ihsnola.org>
2023-04-06 20:29:07 +0000

    circularity 4

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -26,7 +26,7 @@ var init = function (window) {
         // TODO 2 : Create a function that draws a circle 
         function drawCircle(){
              circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
-                physikz.addRandomVelocity(circle, canvas, 20, 3, 50);
+                physikz.addRandomVelocity(circle, canvas, 20, 15, 50);
                     view.addChild(circle);
                         circles.push(circle);
         }
@@ -47,21 +47,15 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-                physikz.updatePosition(circles[0]);
-                physikz.updatePosition(circles[1]);
-                physikz.updatePosition(circles[2]);
-                physikz.updatePosition(circles[3]);
-                physikz.updatePosition(circles[4]);
-            for(var i = 0; circles.length; i++){
-                physikz.updatePosition(circles[i])
+                   
+            for(var i = 0; circles.length > i; i++){
+                physikz.updatePosition(circles[i]);
+                game.checkCirclePosition(circles[i]);
             }
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-                game.checkCirclePosition(circles[0]);
-                game.checkCirclePosition(circles[1]);
-                game.checkCirclePosition(circles[2]);
-                game.checkCirclePosition(circles[3]);
-                game.checkCirclePosition(circles[4]);
+               
+                ////Deleted my hard code because I used iteration!!!!////
 
             // TODO 9 : Iterate over the array
            
